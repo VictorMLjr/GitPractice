bd0f285509ac4e925d6197443d3fea4344625c4e
Victor <narrowblazedelta1@gmail.com>
2026-07-02 20:52:09 -0600

    Added main.cpp

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#using std;
+
+int main()
+{
+    cout << "Hello, Git!" << endl;
+	cout << "This is a simple C++ program." << endl;
+	cout << "I'm now able to use Git to made files and commit them." << endl;
+    return 0;
+}
